33dda5ad691f3249d86929a1c4208f68d0cdf6d4
UNtergas <mycomputerer135@gmail.com>
2024-12-18 14:05:50 +0100

    t

--- config.js
+++ config.js
@@ -61,7 +61,7 @@ const CONFIG = {
 			id: '2',
 			name: 'Gmail',
 			icon: 'mail',
-			link: 'https://mail.google.com/',
+			link: 'https://mail.google.com/mail/u/1/#inbox',
 		},
 		{
 			id: '3',
